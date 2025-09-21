ed16e66f2883d021875f72e76219b6eff353833b
AbdulazizShemsedin <abdulazizshemsedin@gmail.com>
2025-09-21 14:51:03 +0300

    Create a generic data fetching tools

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkel from "./GameCardSkel";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15];
   return (
     <>
@@ -21,7 +21,7 @@ const GameGrid = () => {
               <GameCardSkel key={skeletons} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((data) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,38 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
     id: number
     name: string
 }
 
-interface FetchGenresResponse {
-    count: number
-    results: Genre[]
-}
-
-const useGenre = () => {
-    const [genres, setGenre] = useState<Genre[]>([]);
-      const [error, setError] = useState("");
-      const [isLoading , setLoading] = useState(true)
-    
-      useEffect(() => {
-        const controller = new AbortController()
-    
-        apiClient
-          .get<FetchGenresResponse>("/genres", {signal: controller.signal})
-          .then((res) => {
-            setGenre(res.data.results)
-            setLoading(false)
-          })
-          .catch((err) => {
-            if (err instanceof CanceledError) return
-            setError(err.message)
-            setLoading(false)
-        })
-      }, []);
-      return {genres , error , isLoading}
-}
+const useGenre = () => useData<Genre>('/genres')
 
 export default useGenre
